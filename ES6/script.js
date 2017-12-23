daf138f7eae8ddfa42c7d565f6dd076c92c25be8
aungkaungmyat <amyat@purdue.edu>
2017-12-23 00:12:19 -0500

    the end of tutorial

--- ES6/script.js
+++ ES6/script.js
@@ -27,7 +27,6 @@
     console.log("outside: " + x);
 }
 
-
 window.onload = function(){
 
     var items = document.getElementsByTagName("li");
@@ -80,7 +79,7 @@ window.onload = function(){
 }*/
 
 //for template Strings
-window.onload = function(){
+/*window.onload = function(){
 
     function logNinja(name, age){
         console.log(`my name is ${name} and my age is ${age}`);
@@ -88,4 +87,154 @@ window.onload = function(){
         
     }
     logNinja("Ryu", 24);
+}*/
+
+//for object literals
+/*window.onload = function(){
+    
+    var name = "Crystal";
+    var belt = "Black";
+
+    var ninja = {
+        
+        //ES5
+        // name: name,
+        // belt: belt
+        // chop: function(x){
+        //     console.log(`you chopped the enemy ${x} times`);
+        // }
+        name,belt,
+        chop(x){
+            console.log(`you chopped the enemy ${x} times`);
+        }
+
+    };
+
+    console.log(ninja.name);
+    console.log(ninja.belt);
+    ninja.chop(5);
+
+}*/
+
+//for new string methods
+/*window.onload = function(){
+
+    var str = "graaaaavy ";
+
+    console.log(str.repeat(5));
+
+
+    var youSay = "goodbye";
+
+    if(youSay.startsWith("goodbye")){
+        var iSay = "hello";
+    }
+    console.log(`you say ${youSay}, I say ${iSay}`);
+    // NEW STRING METHOD
+    // -repeat
+    // -startsWith
+    // -endsWith
+    // -includes
+
+}*/
+
+//for arrow functions
+/*window.onload = function(){
+
+    //var ninjaGreeting = name => console.log(`${name} says hiiiiya`);
+    
+    //ninjaGreeting("Mark");
+
+    //for es5
+    var ninja = {
+        name: "Ryu",
+        chop(x){
+            var _this = this; //weird this problem
+            window.setInterval(function(){
+                if(x > 0){
+                    console.log(_this.name + " chopped the enemy");
+                    x--;
+                }
+            }, 1000);
+        }
+    };
+
+    //with arrow fucntion
+    var ninja = {
+        name: "Ryu",
+        chop(x){
+            window.setInterval(() => {
+                if(x > 0){
+                    console.log(this.name + " chopped the enemy");
+                    x--;
+                }
+            }, 1000);
+        }
+    };
+
+    ninja.chop(5);
+}*/
+
+//for sets
+/*window.onload = function(){
+
+    var names = new Set();
+
+    names.add("Shaun").add("Ryu").add("Crystal");
+    names.delete("Crystal");
+    console.log(names.size);
+    console.log(names);
+
+    var ninjas = ["shaun", "crystal", "yoshi", "ryu", "yoshi", "ryu"];
+
+    var refinedNinja = new Set(ninjas);
+    console.log(refinedNinja);
+    
+    ninjas =  [...refinedNinja];
+}*/
+
+//for generators
+window.onload = () => {
+
+    //just seeing how .next and yield work
+    // function* gen(){
+    //     yield console.log("pear");
+    //     yield console.log("banan");
+    //     console.log("apple");
+    // }
+
+    // var myGen = gen();
+    // myGen.next();
+    // myGen.next();
+    
+    //seeing the return object of yield
+    // function* gen(){
+    //     yield "pear";
+    //     yield "banan";
+    //     yield "apple"
+    //     return "all done"; //without return, the value will be undefined.
+    // }
+
+    // var myGen = gen();
+    // console.log(myGen.next());
+    // console.log(myGen.next());
+    // console.log(myGen.next());
+    // console.log(myGen.next());
+
+
+    function* gen(){
+        var x = yield "pear";
+        var y = yield "banan";
+        var z = yield "apple"
+        return x + y + z;
+    }
+
+    var myGen = gen();
+    console.log(myGen.next());
+    console.log(myGen.next(10));
+    console.log(myGen.next(5));
+    console.log(myGen.next(3));
+
+
+    
 }
