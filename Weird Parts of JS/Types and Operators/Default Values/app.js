febb6be077fd9bc4b67c24d6f32479a567cb045e
aungkaungmyat <amyat@purdue.edu>
2018-01-01 00:17:47 -0500

    default value framework

---
+++ Weird Parts of JS/Types and Operators/Default Values/app.js
@@ -0,0 +1,11 @@
+/*function greet(name){
+    name = name || '<Your name here>'; //how to implement default value features in ES6.
+    console.log('Hello ' + name);
+}
+
+greet('Tony');
+greet();*/
+
+
+
+console.log(libraryName);
