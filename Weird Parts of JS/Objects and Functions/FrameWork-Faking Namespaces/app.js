7262f53a0ded8cfa4281dad062ab95e0564d9e6e
aungkaungmyat <amyat@purdue.edu>
2018-01-01 20:17:17 -0500

    JSON vs Object Literals

---
+++ Weird Parts of JS/Objects and Functions/FrameWork-Faking Namespaces/app.js
@@ -0,0 +1,16 @@
+var greet = 'Hello!';
+var greet =  'Hola!';
+
+console.log(greet);
+
+var english = {
+    greetings: {
+        basic: 'Hello!'
+    }
+};
+var spanish = {};
+
+// english.greet = 'Hello!';
+spanish.greet = 'Hola!';
+
+console.log(english);
