3fca9f0c43c68cfad2483165e19c1fcce562f596
aungkaungmyat <amyat@purdue.edu>
2018-01-02 23:50:42 -0500

    objects functions and this

---
+++ Weird Parts of JS/Objects and Functions/Objects, Functions, and 'this'/app.js
@@ -0,0 +1,31 @@
+function a(){
+    console.log(this);
+    this.newvariable = 'hello';
+}
+
+var b = function(){
+    console.log(this);
+}
+
+a();
+console.log(newvariable);
+b();
+
+
+var c ={
+    name: 'The c object',
+    log: function(){
+        var self = this;
+
+        self.name = 'Update c object';
+        console.log(self);
+
+        var setname = function(newname){
+            self.name = newname;
+        }
+        setname('Updated again! The c object');
+        console.log(self);
+    }
+}
+
+c.log();
