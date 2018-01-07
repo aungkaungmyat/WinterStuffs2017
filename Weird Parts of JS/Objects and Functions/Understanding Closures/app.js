5950883a855e9049aa53353e5cb7097dc2033c03
aungkaungmyat <amyat@purdue.edu>
2018-01-07 13:42:35 -0500

    function factories

--- Weird Parts of JS/Objects and Functions/Understanding Closures/app.js
+++ Weird Parts of JS/Objects and Functions/Understanding Closures/app.js
@@ -1,3 +1,4 @@
+//part 1
 function greet(whattosay){
 
     return function(name){
@@ -10,3 +11,60 @@ function greet(whattosay){
 
 var sayHi = greet('Hi');
 sayHi('Tony');
+
+
+
+// part 2
+function buildFunctions(){
+
+    var arr = [];
+
+    for(var i = 0 ; i < 3 ; i++){
+
+        arr.push(
+            function(){
+                console.log(i);
+            }
+        )
+    }
+
+
+    return arr;
+}
+
+var fs = buildFunctions();
+
+fs[0]();  // the value is 3.
+fs[1]();  // the value is 3.
+fs[2]();  // the value is 3.
+
+//to fix the above issue, use 'let' instead of 'var'.
+
+
+//another fix IIFE
+
+function buildFunctions2(){
+
+    var arr = [];
+
+    for(var i = 0 ; i < 3 ; i++){
+
+        arr.push(
+            (function(j){
+                return function(){
+                    console.log(j);
+                }
+                
+            }(i))
+        )
+    }
+
+
+    return arr;
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0]();  // the value is 3.
+fs2[1]();  // the value is 3.
+fs2[2]();  // the value is 3.
