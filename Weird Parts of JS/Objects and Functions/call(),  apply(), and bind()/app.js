6ea1ddd23b0e63932f833a6e551d189fa23f7934
aungkaungmyat <amyat@purdue.edu>
2018-01-07 15:17:13 -0500

    call, apply and bind

---
+++ Weird Parts of JS/Objects and Functions/call(),  apply(), and bind()/app.js
@@ -0,0 +1,57 @@
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    getFullName: function(){
+
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+}
+
+
+var logName = function(lang1, lang2){
+
+    console.log('Logged: ' + this.getFullName());
+    console.log('Argumenst: ' + lang1 + ' ' + lang2);
+    console.log('------------');
+
+} // or add .bind(person) in the end.
+
+var logPersonName = logName.bind(person)
+
+
+logPersonName('en');
+
+logName.call(person, 'en', 'es');
+logName.apply(person, ['en', 'es']);
+
+
+//IIFE way
+(function(lang1, lang2){
+
+    console.log('Logged: ' + this.getFullName());
+    console.log('Argumenst: ' + lang1 + ' ' + lang2);
+    console.log('------------');
+
+}).apply(person, ['en', 'es']);
+
+
+//function borrowing
+var person2 = {
+    firstname: 'Jane',
+    lastname: 'Doe'
+}
+
+console.log(person.getFullName.apply(person2));
+
+
+//function currying
+function multiply(a, b){
+    return a*b;
+}
+
+var multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));
+
+var multiplyByThree = multiply.bind(this, 3);
+console.log(multiplyByThree(4));
