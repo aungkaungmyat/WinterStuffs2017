ee8e0e2b83204a62a1a044a7e3519131a153ae6a
aungkaungmyat <amyat@purdue.edu>
2018-01-08 10:51:07 -0500

    understanding the prototype

---
+++ Weird Parts of JS/Object-Oriented Javascript and Prototypal Inheritance/understanding the prototype/app.js
@@ -0,0 +1,17 @@
+var person =  {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function(){
+        return this.firstname + ' ' + this.lastname;
+    }
+}
+
+
+var john = {
+    firstname: 'Default',
+    lastname: 'Default',
+}
+
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+console.log(john.getFullName());
